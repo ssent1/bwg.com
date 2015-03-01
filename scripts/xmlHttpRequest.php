da801491719b20cd5ab5ba08f5992341161712fe
ssent1 <ssent1@gmail.com>
2015-03-01 18:30:36 -0500

    Initial Commit
    Baseline for project has been completed. Need to make updates as per
    the client’s requests.

---
+++ scripts/xmlHttpRequest.php
@@ -0,0 +1,48 @@
+<?php
+// change the 4 variables below
+$yourName = 'BW Geotextile Web Form';
+$yourEmail = 'jklinn@kw.igs.net';
+$yourSubject = 'New Web Lead';
+$referringPage = '../';
+// no need to change the rest unless you want to. You could add more error checking but I'm gonna do that later in the official release
+
+header('Content-Type: text/xml');
+echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
+
+echo '<resultset>';
+
+function cleanPosUrl ($str) {
+$nStr = $str;
+$nStr = str_replace("**am**","&",$nStr);
+$nStr = str_replace("**pl**","+",$nStr);
+$nStr = str_replace("**eq**","=",$nStr);
+return stripslashes($nStr);
+}
+	if ( $_GET['contact'] == true && $_GET['xml'] == true && isset($_POST['posText']) ) {
+	$to = $yourName;
+	$subject = 'New Web Lead: '.cleanPosUrl($_POST['posRegard']);
+	$message = cleanPosUrl($_POST['posText']);
+	$headers = "From: ".cleanPosUrl($_POST['posName'])." <".cleanPosUrl($_POST['posEmail']).">\r\n";
+	$headers .= 'To: '.$yourName.' <'.$yourEmail.'>'."\r\n";
+	$mailit = mail($to,$subject,$message,$headers);
+
+		if ( @$mailit )
+		{ $posStatus = 'OK'; $posConfirmation = 'Success! Your Email has been sent. We look forward to serving you.'; }
+		else
+		{ $posStatus = 'NOTOK'; $posConfirmation = 'Thanks for sending your message. We look forward to serving you.'; }
+
+		if ( $_POST['selfCC'] == 'send' )
+		{
+		$ccEmail = cleanPosUrl($_POST['posEmail']);
+		@mail($ccEmail,$subject,$message,"From: Yourself <".$ccEmail.">\r\nTo: Yourself");
+		}
+
+	echo '
+		<status>'.$posStatus.'</status>
+		<confirmation>'.$posConfirmation.'</confirmation>
+		<regarding>'.cleanPosUrl($_POST['posRegard']).'</regarding>
+		';
+	}
+echo'	</resultset>';
+
+?>
