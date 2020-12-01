30a4931ab4f9c8d7de0cb82f2d0d1a03d67219ef
Syd Salmon <syd@proganica.com>
2020-12-01 16:06:51 -0500

    Revert "[+] Minified HTML, CSS, JavaScript, and PHP files."
    This reverts commit 3105d89e0e01d8692e554d4e3f4c9ca6f07f68cc.

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,1 +1,26 @@
-<?php $yourName='BW Geotextiles Web Form';$yourEmail='sales@bwgeotextiles.com';$yourSubject='New Web Lead';$referringPage='../';function cleanPosUrl($str){return stripslashes($str);}if(isset($_POST['sendContactEmail'])){$to=$yourEmail;$subject=$yourSubject.': '.$_POST['posRegard'];$message=cleanPosUrl($_POST['posText']);$headers="From: ".cleanPosUrl($_POST['posName'])." <".$_POST['posEmail'].">\r\n";$headers.='To: '.$yourName.' <'.$yourEmail.'>'."\r\n";$mailit=mail($to,$subject,$message,$headers);if(@$mailit){header('Location: '.$referringPage.'?success=true');}else{header('Location: '.$referringPage.'?error=true');}} ?>
+<?php
+// Change the 4 variables below
+$yourName = 'BW Geotextiles Web Form';
+$yourEmail = 'sales@bwgeotextiles.com';
+$yourSubject = 'New Web Lead';
+$referringPage = '../';
+// No need to edit below unless you really want to. It's using a simple php mail() function. Use your own if you want
+function cleanPosUrl ($str) {
+return stripslashes($str);
+}
+	if ( isset($_POST['sendContactEmail']) )
+	{
+	$to = $yourEmail;
+	$subject = $yourSubject.': '.$_POST['posRegard'];
+	$message = cleanPosUrl($_POST['posText']);
+	$headers = "From: ".cleanPosUrl($_POST['posName'])." <".$_POST['posEmail'].">\r\n";
+	$headers .= 'To: '.$yourName.' <'.$yourEmail.'>'."\r\n";
+	$mailit = mail($to,$subject,$message,$headers);
+		if ( @$mailit ) {
+		header('Location: '.$referringPage.'?success=true');
+		}
+		else {
+		header('Location: '.$referringPage.'?error=true');
+		}
+	}
+?>
