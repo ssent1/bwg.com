e1411536f15dd9469c0c9aa1e884e5ad754ddaeb
Syd Salmon <syd@proganica.com>
2020-12-14 21:36:12 -0500

    [Δ] minimized JavaScript files.

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,12 +1,1 @@
-var $ = jQuery.noConflict();
-$(document).ready(function() {
-		/* for top navigation */
-		$(" .navigation ul ").css({display: "none"}); // Opera Fix
-		$(" .navigation li").hover(function(){
-		$(this).find('ul:first').css({visibility: "visible",display: "none"}).slideDown(400);
-		},function(){
-		$(this).find('ul:first').css({visibility: "hidden"});
-		});
-		
-});		 
-	
+var $=jQuery.noConflict();$(document).ready(function (){$(" .navigation ul ").css({display: "none"}); $(" .navigation li").hover( function (){$(this) .find("ul:first") .css({visibility: "visible", display: "none"}) .slideDown(400);}, function (){$(this).find("ul:first").css({visibility: "hidden"});});});
